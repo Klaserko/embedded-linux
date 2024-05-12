c95cc226dde2c6a2b8442596456abde0648e0ee0
Richard Žember <richard@hamso.dk>
2024-05-12 15:04:54 +0200

    Update

--- src/main.sh
+++ src/main.sh
@@ -3,7 +3,6 @@
 check_script="./check_connection.sh"
 download_script="./new_download_files.sh"
 annotation_script="./annotate_images.sh"
-server_website="192.168.10.1:8080"
 git_script="./git_push.sh"
 logging_script="./log_wifi_link.sh"
 
@@ -12,17 +11,14 @@ sleep_time=6
 # Activate the logging script
 $logging_script $check_script &
 
-downloaded=false
 
 while true; do
     if current_wifi=$("$check_script"); then
+        echo "Connected to $current_wifi, starting download"
         $download_script "../images/$current_wifi"
-        downloaded=true
-    fi
 
-    if [ "$downloaded" = true ]; then
-        # Annotate the images
-        $annotation_script "../images/$current_wifi/$server_website"
+        echo "Download finished, starting annotation"
+        $annotation_script "../images/$current_wifi"
 
         echo "Annotation finished, pushing to git"
         $git_script
